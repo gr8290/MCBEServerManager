76b3454dc2aa64496324de81b85a8b7486bedad5
r-goto <gr8290@gmail.com>
2021-05-08 20:20:32 +0900

    Properties読み込み機能追加

---
+++ ServerPropertiesManager/Main.cs
@@ -0,0 +1,31 @@
+﻿using ServerPropertiesManager.Util;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerPropertiesManager
+{
+    public class Main
+    {
+        IDictionary<string, string> _Properties = null;
+
+        public Main(string propertiesFilePath)
+        {
+            using (TextReader reader = File.OpenText(propertiesFilePath))
+            {
+                _Properties = PropertiesLoader.Load(reader);
+            }
+        }
+
+        public IDictionary<string, string> GetProperties()
+        {
+            return _Properties;
+        }
+
+  
+
+    }
+}
